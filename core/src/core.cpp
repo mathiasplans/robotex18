20678186d60b94a0904f6ddd43c3faa5e2d3d2fc
Mathias Plans <mathiasplans15@gmail.com>
2018-09-16 20:23:06 +0300

    Create a subscriber for "ball" topic in core

--- core/src/core.cpp
+++ core/src/core.cpp
@@ -1,5 +1,32 @@
+#include "ros/ros.h"
+#include "core/Ball.h"
+
 #include "statemachine.hpp"
+
+/**
+ * Handles
+ */
+void vision_callback(const core::Ball::ConstPtr& msg){
+  ROS_INFO("I heard: [%d, %d]", msg->ballX, msg->width);
+}
+
+
 int main(){
+
+  // Initialize ROS
+  ros::init(argc, argv, "core");
+
+  // Handle for the specific node
+  ros::NodeHandle n;
+
+  // Subscribe to a message from vision
+  ros::Subscriber sub = n.subscribe("ball", 1000, vision_callback);
+
+  // Initialize the CORE
   StateMachine();
+
+  // Run ROS
+  ros::spin();
+
   return 0;
 }
