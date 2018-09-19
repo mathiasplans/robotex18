3860764ac2ab48739d10eed0ef21934e98584089
Adrian Kirikal <kirikaladrian@gmail.com>
2018-09-19 13:20:41 +0300

    Add port forward for web server access

--- port_forward.sh
+++ port_forward.sh
@@ -1,2 +1,3 @@
-# Forwards local port 2222 to remote port 12312 in an aws server. Usage: ssh -p 2222 robot@localhost
+# Forwards local port 2222 to remote port 12312 in an aws server.  Usage: ssh -p 2222 robot@localhost
 ssh -fNT -L 2222:localhost:12312 ec2-user@18.184.199.20 -i ~/robot_bridge.pem
+ssh -fNT -L 3333:localhost:12313 ec2-user@18.184.199.20 -i ~/robot_bridge.pem
